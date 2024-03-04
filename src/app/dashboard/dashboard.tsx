ea32c9c0d199530e46bb01e29e86cc1e53bb444e
AnthonyG98 <gonz.anthony6@gmail.com>
2024-03-04 14:27:26 -0800

    front end dashboard header

--- src/app/dashboard/dashboard.tsx
+++ src/app/dashboard/dashboard.tsx
@@ -1,5 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCubes } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCubes,
+  faMagnifyingGlass,
+  faHome,
+  faGlobeAmericas,
+  faUserFriends,
+  faBell,
+} from "@fortawesome/free-solid-svg-icons";
 import "./dashstyles.css";
 export default function Dashboard() {
   return (
@@ -7,7 +14,38 @@ export default function Dashboard() {
       <div className="dashboard-container">
         <div className="dashboard-col1-container">
           <div className="logo" id="dash-logo">
-            <FontAwesomeIcon id="logo-icon" icon={faCubes} />
+            <FontAwesomeIcon className="head-icon" icon={faCubes} />
+          </div>
+          <div className="dash-col1-row" id="header-icons">
+            <div className="col1-row-icon-container">
+              <FontAwesomeIcon className="head-icon" icon={faHome} />
+              <div className="notif">3</div>
+            </div>
+            <div className="col1-row-icon-container">
+              <FontAwesomeIcon className="head-icon" icon={faGlobeAmericas} />
+              <div className="notif">1</div>
+            </div>
+            <div className="col1-row-icon-container">
+              <FontAwesomeIcon className="head-icon" icon={faUserFriends} />
+              <div className="notif">1</div>
+            </div>
+            <div className="col1-row-icon-container">
+              <FontAwesomeIcon className="head-icon" icon={faBell} />
+              <div className="notif">2</div>
+            </div>
+          </div>
+          <div className="dash-col1-row" id="search-container">
+            <FontAwesomeIcon
+              className="head-icon"
+              id="magnifying-glass"
+              icon={faMagnifyingGlass}
+            />
+
+            <input
+              className="input-field"
+              id="search-input"
+              placeholder="Search user, list, or entertainment"
+            />
           </div>
         </div>
       </div>
